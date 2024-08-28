89f1fdc1ca3456bd4e08affb3e41951d2fc8398c
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-29 05:51:54 +0700

    todo form setup

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const TodoForm = () => {
   return (
-    <div>TodoForm</div>
-  )
-}
+    <form className="TodoForm">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="What is the task today?"
+      />
+      <button type="submit" className="todo-btn"></button>
+    </form>
+  );
+};
