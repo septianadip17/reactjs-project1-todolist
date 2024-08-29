0220959a4a43eef1ee1881a9fc4df9225c01a3ae
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-30 05:26:29 +0700

    todos

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
-uuidv4()
+import { Todo } from "./Todo";
+uuidv4();
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
@@ -15,6 +16,10 @@ export const TodoWrapper = () => {
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
+      <Todo />
     </div>
   );
 };
