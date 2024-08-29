8fc59f6d9d97282ed942e47a5972761639facf32
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-30 05:50:35 +0700

    toggle completed

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -6,6 +6,7 @@ uuidv4();
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
+
   const addTodo = (todo) => {
     setTodos([
       ...todos,
@@ -13,12 +14,21 @@ export const TodoWrapper = () => {
     ]);
     console.log(todos);
   };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
       ))}
     </div>
   );
