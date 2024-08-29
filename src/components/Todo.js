bdc513f7e0109d32ca1402c90ccdd286d9d46b45
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-30 06:21:02 +0700

    edit task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const Todo = ({ task, toggleComplete, deleteTodo }) => {
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
   return (
     <div className="Todo">
       <p
@@ -13,7 +13,7 @@ export const Todo = ({ task, toggleComplete, deleteTodo }) => {
         {task.task}
       </p>
       <div>
-        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
         <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
       </div>
     </div>
